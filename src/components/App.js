e098989ed6bf260b4c2213fa5cd159685d63fd47
chelkuhs <aurelioarcabascio@gmail.com>
2019-07-30 17:11:45 -0400

    guild cards finally render

--- src/components/App.js
+++ src/components/App.js
@@ -35,49 +35,41 @@ class App extends React.Component {
   guildInfo() {
     Fire.firestore().collection('guilds')
       .get().then(snapshot => {
-        snapshot.forEach(doc => {
-          this.setState({
-            guilds: [{
+        let guilds = []
+          snapshot.forEach(doc => {
+            guilds.push({
               id: doc.id,
               guildDesc: doc.data().guildDesc,
               guildFaction: doc.data().guildFaction,
               guildName: doc.data().guildName,
               guildRegion: doc.data().guildRegion,
               guildServer: doc.data().guildServer
-            }]
+            })
           })
+          this.setState({guilds}, () => console.log(this.state))
         })
-      })
   }
 
   renderCards() {
-    let guilds = this.state.guilds
-    guilds.map(guild => {
+    var guildFaction
+    var guildComp = this.state.guilds.map(guild => {
+      if(guild.guildFaction === 'Alliance') {
+        guildFaction = 'guild-name -alliance'
+      } else if (guild.guildFaction === 'Horde') {
+        guildFaction = 'guild-name -horde'
+      }
       console.log(guild)
-      return <Guilds
-              key={guild.id}
-              guildFaction={guild.guildFaction} 
-              guildServer={guild.guildServer}
-              guildName={guild.guildName} 
-              guildDesc={guild.guildDesc} />
-    })
+      return <Guilds key={guild.id} guildFaction={guildFaction} guildServer={guild.guildServer} guildName={guild.guildName} guildRegion={guild.guildRegion} guildDesc={guild.guildDesc} />
+            })
+            return <CardDeck>{guildComp}</CardDeck>
   }
 
-  render() {
-    const alliance = 'guild-name -alliance'
-    const horde = 'guild-name -horde'       
+  render() {  
     return(
       <div className="main-div">
-        <CardDeck>
-          <Guilds guildFaction={alliance} guildServer="Sargeras" guildRegion="US-" guildName="HelloQ" guildDesc="This is a test description." />
-          <Guilds guildFaction={horde} guildServer="Sargeras" guildRegion="US-" guildName="HelloQ" guildDesc="This is a test description." />
-          <Guilds guildFaction={alliance} guildServer="Sargeras" guildRegion="US-" guildName="HelloQ" guildDesc="This is a test description." />
-        </CardDeck>
-          <Directory />
-        <CardDeck>
-          {this.renderCards()}
-        </CardDeck>
-    </div>
+      <Directory />
+        {this.renderCards()}
+      </div>
     )
   }
 }
